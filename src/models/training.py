e6318923995fbddaf9bd300c78b73e67c1ac5510
Claudio <claudio.pereti@outlook.com>
2021-06-06 16:33:33 +0200

    added main() function

--- src/models/training.py
+++ src/models/training.py
@@ -8,17 +8,20 @@ from load_data import load_pokemon_array
 from build_model import VarationalConvAE,Encoder,Decoder
 
 
+def main():
+    # call encoder and decoder
+    encoder_model = Encoder(encoder)
+    decoder_model = Decoder(decoder)
 
-# call encoder and decoder
-encoder_model = Encoder(encoder)
-decoder_model = Decoder(decoder)
-
-# call the variation autoencoder
-vae = VarationalConvAE(encoder_model,decoder_model)
+    # call the variation autoencoder
+    vae = VarationalConvAE(encoder_model,decoder_model)
 
 
-vae.compile(optimizer = vae_optimizer)
-pokemon_array = load_pokemon_array()
+    vae.compile(optimizer = vae_optimizer)
+    pokemon_array = load_pokemon_array()
 
-pokemon_array = pokemon_array[0:train_size,:,:,:]
-vae.fit(pokemon_array,**train)
+    pokemon_array = pokemon_array[0:train_size,:,:,:]
+    vae.fit(pokemon_array,**train)
+
+if __name__ == "__main__":
+    main()
