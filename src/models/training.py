53335616c79f81a8afe2eb7e87334ac1d1e34835
Claudio <claudio.pereti@outlook.com>
2021-06-08 14:51:16 +0200

    cleaned trainig

--- src/models/training.py
+++ src/models/training.py
@@ -5,23 +5,28 @@ sys.path.append('../../config')
 import config
 from config import train, train_size, vae_optimizer, encoder, decoder
 from load_data import load_pokemon_array
-from build_model import VarationalConvAE,Encoder,Decoder
+from build_model import VarationalConvAE,Encoder,Decoder,Sampling
+from tensorflow import saved_model
+from tensorflow.keras.models import load_model
 
 
 def main():
-    # call encoder and decoder
-    encoder_model = Encoder(encoder)
-    decoder_model = Decoder(decoder)
 
-    # call the variation autoencoder
-    vae = VarationalConvAE(encoder_model,decoder_model)
-
-
+    # Call the model
+    vae = VarationalConvAE()
+    # Compile the model
     vae.compile(optimizer = vae_optimizer)
+    # Load processed Dataset
     pokemon_array = load_pokemon_array()
-
+    # Slice dataset to train_size
     pokemon_array = pokemon_array[0:train_size,:,:,:]
+    # Train model
     vae.fit(pokemon_array,**train)
+    # Save model
+    vae.save('../../model/')
+
+
+
 
 if __name__ == "__main__":
     main()
